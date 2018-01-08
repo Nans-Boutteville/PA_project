b4bcf6e0d617092a9dc437e04ad66b3c6de59c89
Daniel Kuchta <daniel.kuchta@yahoo.fr>
2018-01-08 19:58:55 +0100

    - plugins d'affichage (méthode dessiner() ) et d'attaque (méthode attaquer() )

---
+++ robot_wars/src/main/java/plugins_basique/Robot_affichage_plugins.java
@@ -0,0 +1,23 @@
+package plugins_basique;
+
+import annotation.Dessiner;
+import annotation.Graphique;
+
+import java.awt.*;
+import java.util.Random;
+
+@Graphique
+public class Robot_affichage_plugins {
+
+    @Dessiner
+    public void dessiner(Graphics g,Point p){
+        Random rand = new Random();
+        float r = rand.nextFloat();
+        float gr = rand.nextFloat();
+        float b = rand.nextFloat();
+        Color c = g.getColor();
+        g.setColor(new Color(r,gr,b));
+        g.fillRect(10,10,80,80);
+        g.setColor(c);
+    }
+}
---
+++ robot_wars/src/main/java/plugins_basique/Robot_attaque_plugins.java
@@ -0,0 +1,27 @@
+package plugins_basique;
+
+import annotation.Attaque;
+
+import java.awt.*;
+
+@Attaque(perteEnergie = 10, perteVie = 5)
+public class Robot_attaque_plugins {
+    private int portee = 10;
+
+    public boolean attaque(Point p1, Point p2, Graphics g) {
+        boolean returnF = false;
+        if (p1.getX() > p2.getX()) {
+            if (p1.getX() - p2.getX() < 10) {
+                returnF = true;
+            }
+        }
+        if (returnF) {
+            annimation(p1,p2,g);
+        }
+        return returnF;
+    }
+
+    public void annimation(Point p1,Point p2,Graphics g){
+        //fonction qui fait l'annimation entre les deux robots
+    }
+}
