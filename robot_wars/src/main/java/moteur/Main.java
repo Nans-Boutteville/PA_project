d15481eb105d22677a12fb012faa6908e9266370
Adrien Vray <vray.adrien@gmail.com>
2018-01-08 20:27:35 +0100

    Affichage du rectangle (robot)

--- robot_wars/src/main/java/moteur/Main.java
+++ robot_wars/src/main/java/moteur/Main.java
@@ -4,9 +4,10 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main {
 
-	public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+	public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, InterruptedException {
         Robot t = new Robot();
         Vue v = new Vue();
+        Thread.currentThread().sleep(1000);
         t.seDessiner(v.getG());
     }
 
--- robot_wars/src/main/java/moteur/Vue.java
+++ robot_wars/src/main/java/moteur/Vue.java
@@ -6,24 +6,22 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class Vue{
+public class Vue extends JPanel{
     Graphics g;
 
     public Vue(){
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        frame.add(panel, BorderLayout.CENTER );
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Robot Wars Ultimate !");
         frame.setSize(600,600);
         frame.setLocationRelativeTo(null);
+        frame.add(panel, BorderLayout.CENTER );
         frame.setVisible(true);
         this.g = panel.getGraphics();
-        System.out.println(g);
     }
 
     public Graphics getG() {
-        System.out.println(g);
         return g;
     }
 }
