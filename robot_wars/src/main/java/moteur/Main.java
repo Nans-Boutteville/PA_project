c71aeda1decf3e782cfe9ad35e2ccbac04c47a96
Adrien Vray <vray.adrien@gmail.com>
2018-01-09 15:00:31 +0100

    Utilisation des IA

--- robot_wars/src/main/java/moteur/Main.java
+++ robot_wars/src/main/java/moteur/Main.java
@@ -7,7 +7,9 @@ public class Main {
 	public static void main(String[] args) throws InvocationTargetException, IllegalAccessException, InterruptedException {
         Vue v = new Vue();
         Thread.currentThread().sleep(1000);
-        Moteur m  = new Moteur(v.getG());
+        Moteur m = new Moteur(v.getG());
+        IAsimple player1 = new IAsimple(m.getR1(), m.getR2d2());
+        IAsimple player2 = new IAsimple(m.getR2d2(), m.getR1());
+        m.run(player1, player2);
     }
-
 }
