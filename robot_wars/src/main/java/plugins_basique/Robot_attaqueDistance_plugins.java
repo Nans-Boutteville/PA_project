026f49fa6fff3b68e5b964e33b9fbe18d65d14fb
Daniel Kuchta <daniel.kuchta@yahoo.fr>
2018-01-10 23:48:32 +0100

    - plugin attaque à distance testé dans plugin attaque - ajout des images

--- robot_wars/src/main/java/plugins_basique/Robot_attaqueDistance_plugins.java
+++ robot_wars/src/main/java/plugins_basique/Robot_attaqueDistance_plugins.java
@@ -7,9 +7,10 @@ import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-@Attaque(perteEnergie = 10, perteVie = 5)
-public class Robot_attaque_plugins {
-    private int portee = 20;
+@Attaque(perteEnergie = 30, perteVie = 5)
+public class Robot_attaqueDistance_plugins {
+
+    private int portee = 70;
 
     public boolean attaque(Point p1, Point p2, Graphics g) {
         boolean returnF = false;
@@ -30,24 +31,6 @@ public class Robot_attaque_plugins {
     }
 
     public void annimation(Point p1,Point p2,Graphics g){
-        /*double newX=0;
-        double newY=0;
-        newX=(p1.getX()+p2.getX())/2;
-        newY=(p1.getY()+p2.getY())/2;
-
-        try {
-            Image img = ImageIO.read(new File("images/explosion3.png"));
-            g.drawImage(img,(int) newX,(int)newY,50,50,null);
-        } catch (IOException e) {
-            System.out.println("n'affiche pas l'attaque");
-        }
-
-        try {
-            Thread.currentThread().sleep(5);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }*/
-
 
 
         g.setColor(Color.RED);
